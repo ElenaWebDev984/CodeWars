73fa80d0809d738d5f2627b454e47fbf7b05d36f
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-04-17 15:37:36 +0200

    added helloWorld

---
+++ 8kyu/function1-helloWorld.js
@@ -0,0 +1,20 @@
+// 🎯 Function 1 - hello world
+
+//❓ DESCRIPTIONS
+// Make a simple function called greet that returns the most-famous "hello world!".
+//
+//     Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+// 📌 TESTS
+//assert.strictEqual(greet(), "hello world!");
+
+// ✅ SOLUTIONS
+// 1 variant
+const greet = (str) => 'hello world!'
+
+// 2 variant
+function greet() {
+    return 'hello world!';
+}
+
