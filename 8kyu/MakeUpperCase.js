f87db252a3f2e81121184d80c6e60f8e73ead70d
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-19 22:09:50 +0200

    completed MakeUpperCase

---
+++ 8kyu/MakeUpperCase.js
@@ -0,0 +1,18 @@
+// 🎯 MakeUpperCase
+
+//❓ DESCRIPTIONS
+// Write a function which converts the input string to uppercase.
+
+// // 📌 TESTS
+// assert.strictEqual(makeUpperCase(""),           "");
+// assert.strictEqual(makeUpperCase("hello"), "HELLO");
+// assert.strictEqual(makeUpperCase("Hello"), "HELLO");
+// assert.strictEqual(makeUpperCase("HELLO"), "HELLO");
+
+
+// ✅ SOLUTIONS
+
+const makeUpperCase = (str) => str.toUpperCase();
+
+
+
