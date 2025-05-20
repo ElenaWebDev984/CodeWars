8ec2dd85cfb504cb912394f764c861b12715ce21
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-20 17:47:39 +0200

    added Multiply TS

---
+++ 8kyu/multiply.ts
@@ -0,0 +1,19 @@
+// 🎯 Multiply
+
+//❓ DESCRIPTIONS
+// This code does not execute properly. Try to figure out why.
+
+// 📌 TESTS
+// assert.strictEqual(multiply(1,1), 1);
+// assert.strictEqual(multiply(2,1), 2);
+// assert.strictEqual(multiply(2,2), 4);
+// assert.strictEqual(multiply(3,5), 15); 
+// assert.strictEqual(multiply(5,0), 0);
+// assert.strictEqual(multiply(0,5), 0);
+// assert.strictEqual(multiply(0,0), 0); 
+
+// ✅ SOLUTIONS
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
