223e2da1aad7b9101ae298b7d38de956c78cdf90
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-20 17:52:46 +0200

    added Multiply JS

---
+++ 8kyu/multiply_TS.js
@@ -0,0 +1,19 @@
+// 🎯 Multiply
+
+//❓ DESCRIPTIONS
+// This code does not execute properly. Try to figure out why.
+
+// 📌 TESTS
+// assert.strictEqual(multiply(1,1), 1);
+// assert.strictEqual(multiply(2,1), 2);
+// assert.strictEqual(multiply(2,2), 4);
+// assert.strictEqual(multiply(3,5), 15); 
+// assert.strictEqual(multiply(5,0), 0);
+// assert.strictEqual(multiply(0,5), 0);
+// assert.strictEqual(multiply(0,0), 0); 
+
+// ✅ SOLUTIONS
+
+function multiply_TS(a, b) {
+    return a * b; // Explicitly return the result of the multiplication
+}
