cc8e1c7da08aba35c2856b39ca8c52a34e993354
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-05-25 20:13:32 +0200

    completed ConvertANumberToAString_TS

---
+++ 8kyu/ConvertANumberToAString_TS.ts
@@ -0,0 +1,30 @@
+// 🎯 Convert a Number to a String!
+
+//❓ DESCRIPTIONS
+// We need a function that can transform a number (integer) into a string.
+//
+// What ways of achieving this do you know?
+//
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+// 📌 TESTS
+// test.assert_equals(number_to_string(67), '67')
+// test.assert_equals(number_to_string(79585), '79585')
+// test.assert_equals(number_to_string(-79585), '-79585')
+// test.assert_equals(number_to_string(1+2), '3')
+// test.assert_equals(number_to_string(1-2), '-1')
+// test.assert_equals(number_to_string(0), '0')
+
+// ✅ SOLUTIONS
+// 1 variant
+export function numberToString(num: number): string {
+    return num.toString();
+}
+
+// 2 variant
+
+
+// 3 variant
